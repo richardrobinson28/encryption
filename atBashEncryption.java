c054731b0df209a9ab123d2dd40b548f72a808e9
Ricky Robb <togiraikonoka2@users.noreply.github.com>
2017-03-24 23:10:22 +0000

    More optimization
    less code, same amount of work

--- atBashEncryption.java
+++ atBashEncryption.java
@@ -4,12 +4,11 @@ public class atBashEncryption {
 	private static Scanner	in	= new Scanner(System.in), input = new Scanner(System.in);
 	private static String	encrypt, decrypt, output = "", alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 !\"£$%^&*()-=+,./\\",
 	        atBash = "MNOPQRSTUVWXYZ67890ABCDEFG!\"£$HI12345JKLfghijklmno%^&*()-=+,./\\pq rstuvwxyzabcde";
-	private static int		i, j, sCase, aLen, bashLen = atBash.length();
+	private static int		i, j, aLen, bashLen = atBash.length();
 
 	public static void main(String args[]) {
 		System.out.println("Do you want to: " + "\n1: encrypt" + "\n2: decrypt");
-		sCase = in.nextInt();
-		switchCase(sCase);
+		switchCase(in.nextInt());
 		in.close();
 	}
 
@@ -21,14 +20,12 @@ public class atBashEncryption {
 				aLen = encrypt.length();
 				System.out.println(encrypt(encrypt, aLen));
 				break;
-			// return encrypt;
 			case 2:
 				System.out.println("Please enter what you want to decrypt");
 				decrypt = input.nextLine();
 				aLen = decrypt.length();
 				System.out.println(decrypt(decrypt, aLen));
 				break;
-			// return decrypt;
 			default:
 				System.out.println("Please choose either encrypt or decrypt");
 				break;
@@ -37,22 +34,21 @@ public class atBashEncryption {
 		return "";
 	}
 
-	private static String encrypt(String a, int b) {
-		for (i = 0; i < b; i++) {
+	private static String encrypt(String b, int c) {
+		for (i = 0; i < c; i++) {
 			for (j = 0; j < bashLen; j++) {
-				if (a.charAt(i) == alphabet.charAt(j)) {
+				if (b.charAt(i) == alphabet.charAt(j)) {
 					output += atBash.charAt(j);
 				}
 			}
 		}
-
 		return output;
 	}
 
-	private static String decrypt(String a, int b) {
-		for (i = 0; i < b; i++) {
+	private static String decrypt(String b, int c) {
+		for (i = 0; i < c; i++) {
 			for (j = 0; j < bashLen; j++) {
-				if (a.charAt(i) == atBash.charAt(j)) {
+				if (b.charAt(i) == atBash.charAt(j)) {
 					output += alphabet.charAt(j);
 				}
 			}
